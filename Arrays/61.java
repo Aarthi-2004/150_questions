d52d75f3e12e7bf09ad870f4612b2dc2e096b2af
Aarthi.S <168005532+Aarthi-2004@users.noreply.github.com>
2025-06-23 18:29:05 +0530

    Add files via upload

---
+++ Arrays/61.java
@@ -0,0 +1,17 @@
+//Group Reverse
+//int a[] = { 10, 20, 30, 40, 50, 25, 35, 45 }; d=2
+//Expected OP: 30 20 10 45 35 25 50 40
+import java.util.*;
+ class GroupReverse {
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30, 40, 50, 25, 35, 45};
+        int d = 3;
+        int n = a.length;
+        for (int i = 0; i <n; i += d) {
+            int end = Math.min(i+d-1,n-1);
+           for (int j = end; j >= i; j--) {
+                System.out.print(a[j] + " ");
+            }
+        }
+    }
+}
---
+++ Arrays/64.java
@@ -0,0 +1,32 @@
+//Program to find the frequency count of every number in an array in descending order
+//Input={40,20,10,50,20,10,30,40}
+//40->2
+//20->2
+//10->2
+//50->1
+//30->1
+import java.util.*;
+ FrequencyByDescendingCount {
+    public static void main(String[] args) {
+        int[] arr = {40, 20, 10, 50, 20, 10, 30, 40};
+
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : arr) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(freqMap.entrySet());
+
+        list.sort((e1, e2) -> {
+            if (!e1.getValue().equals(e2.getValue())) {
+                return e2.getValue() - e1.getValue();
+            } else {
+                return e2.getKey() - e1.getKey();  
+            }
+        });
+
+        for (Map.Entry<Integer, Integer> entry : list) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+    }
+}
