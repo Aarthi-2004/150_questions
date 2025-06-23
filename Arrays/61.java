3f4f2212e74c0d504271eac3d07ef63714c7bd4b
Aarthi.S <168005532+Aarthi-2004@users.noreply.github.com>
2025-06-23 18:29:37 +0530

    Update 61.java

--- Arrays/61.java
+++ Arrays/61.java
@@ -1,5 +1,5 @@
 //Group Reverse
-//int a[] = { 10, 20, 30, 40, 50, 25, 35, 45 }; d=2
+//int a[] = { 10, 20, 30, 40, 50, 25, 35, 45 }; d=3
 //Expected OP: 30 20 10 45 35 25 50 40
 import java.util.*;
  class GroupReverse {
