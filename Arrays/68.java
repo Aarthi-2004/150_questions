c121f013749215d7d95f3234234270db5a24207d
Aarthi.S <168005532+Aarthi-2004@users.noreply.github.com>
2025-06-24 18:48:06 +0530

    68.java

--- Arrays/68.java
+++ Arrays/68.java
@@ -4,26 +4,29 @@
 // O/p: 1
 import java.util.*;
 class Main{
-    public static void main(String[] args){
-        int[] arr = {1,2,3,1,3,4,2,5,6,1};
-        System.out.println(call(arr));
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 1, 3, 4, 2, 5, 6, 1};
+        System.out.println("Output: " + findLongestDistanceDuplicate(arr));
     }
-    private static int call(int[] arr){
-        int max = -1;
-        int res = -1;
-        HashMap<Integer, Integer> fd = new HashMap<>();
-        for(int i = 0; i < arr.length; i++){
-            if(fd.containsKey(arr[i])){
-                int d = i - fd.get(arr[i]);
-                if(d > max){
-                    max = d;
-                    res = arr[i];
+
+    public static int findLongestDistanceDuplicate(int[] arr) {
+        int maxDistance = 0;
+        int result = -1;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = arr.length - 1; j > i; j--) {
+                if (arr[i] == arr[j]) {
+                    int distance = j - i;
+                    if (distance > maxDistance) {
+                        maxDistance = distance;
+                        result = arr[i];
+                    }
+                    break; 
                 }
             }
-            else{
-                fd.put(arr[i], i);
-            }
         }
-        return res;
-        }
+
+        return result;
     }
+}
+
