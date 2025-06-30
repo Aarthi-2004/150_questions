6d5030098a34b64d1364141f43de3eae0ef47584
Aarthi.S <168005532+Aarthi-2004@users.noreply.github.com>
2025-06-30 18:14:54 +0530

    Add files via upload

---
+++ Arrays/80.java
@@ -0,0 +1,17 @@
+//Input: arr[] = [16, 17, 4, 3, 5, 2]
+//Output: [17 5 2]
+class Leaders{
+    public static void main(String[] args){
+        int[] arr = {16,17,4,3,5,2};
+        int max = 0;
+        StringBuilder res = new StringBuilder();
+        for(int i = arr.length - 1; i >= 0; i--){
+            if(arr[i] > max){
+                max = arr[i];
+                res.insert(0,max + " ");
+            }
+        }
+        System.out.println(res);
+    }
+    //O(N) Solution Approach
+}
